f6470a838f22a6aa38f40adef8dd2184c0354de3
Tornike Tsagareishvili <dht5152@gmail.com>
2025-05-07 21:30:46 +0400

    Add: Navigation by category click

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -1,15 +1,28 @@
 "use client";
 
+import { useState, useEffect } from "react";
+import { useRouter, usePathname, useSearchParams } from "next/navigation";
 import { useGetAllProducts } from "@/hooks/useGetAllProducts";
 
 import "./Sidebar.css";
 
 export default function Sidebar() {
+  const router = useRouter();
+  const pathname = usePathname();
+  const searchParams = useSearchParams();
   const { products } = useGetAllProducts();
+
+  const [selectedCategory, setSelectedCategory] = useState(
+    searchParams.get("category") || null
+  );
+
+  useEffect(() => {
+    setSelectedCategory(searchParams.get("category") || null);
+  }, [searchParams]);
 
   const upperCaseStores = () => {
     const stores = products?.map((store) => {
-      return store.store;
+      return store.category;
     });
 
     const set = new Set(stores);
@@ -21,13 +34,32 @@ export default function Sidebar() {
     return upperCaseStores;
   };
 
+  const handleCategoryClick = (store: string) => {
+    const categoryParam = store.toLowerCase().replace(/ /g, "_");
+
+    const params = new URLSearchParams(searchParams);
+    params.set("category", categoryParam);
+
+    setSelectedCategory(store);
+
+    const url = `${pathname}?${params.toString()}`;
+    router.push(url);
+  };
+
   return (
     <aside className="sidebar">
       <h2>Stores</h2>
       <ul className="category-list">
         {upperCaseStores()?.map((store, index) => (
           <li key={index} className="category-item">
-            <a className="category-link">{store}</a>
+            <a
+              className={`category-link ${
+                selectedCategory === store ? "active" : ""
+              }`}
+              onClick={() => handleCategoryClick(store)}
+            >
+              {store}
+            </a>
           </li>
         ))}
       </ul>
