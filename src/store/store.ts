822748264c8c6c9507dd4c4acc59b20aec3ae058
Tornike Tsagareishvili <dht5152@gmail.com>
2025-05-18 14:29:10 +0400

    Drop: Add To Cart in from redux

--- src/store/store.ts
+++ src/store/store.ts
@@ -5,7 +5,6 @@ export interface AppState {
   sort: string;
   term: string;
   searchKey: string;
-  addToCart: string[];
   showToast: boolean;
 }
 
@@ -13,7 +12,6 @@ export const CATEGORY = "CATEGORY";
 export const SORT = "SORT";
 export const TERM = "TERM";
 export const SEARCH_KEY = "SEARCH_KEY";
-export const ADD_TO_CART = "ADD_TO_CART";
 export const SHOW_TOAST = "SHOW_TOAST";
 
 interface IncrementAction extends Action<typeof CATEGORY> {
@@ -28,9 +26,7 @@ interface TERM extends Action<typeof TERM> {
 interface SEARCH_KEY extends Action<typeof SEARCH_KEY> {
   payload: string;
 }
-interface ADD_TO_CART extends Action<typeof ADD_TO_CART> {
-  payload: string[];
-}
+
 interface SHOW_TOAST extends Action<typeof SHOW_TOAST> {
   payload: boolean;
 }
@@ -40,15 +36,13 @@ export type AppAction =
   | SORT
   | TERM
   | SEARCH_KEY
-  | ADD_TO_CART
-  | SHOW_TOAST;
+\  | SHOW_TOAST;
 
 const initialState: AppState = {
   currentCategory: "",
   sort: "discount-percentage-desc",
   term: "",
   searchKey: "",
-  addToCart: [],
   showToast: false,
 };
 
@@ -62,10 +56,6 @@ function reducer(state = initialState, action: AppAction): AppState {
       return { ...state, term: action.payload };
     case SEARCH_KEY:
       return { ...state, searchKey: action.payload };
-    case ADD_TO_CART:
-      const payloadItems =
-        typeof action.payload === "string" ? [action.payload] : action.payload;
-      return { ...state, addToCart: [...state.addToCart, ...payloadItems] };
     case SHOW_TOAST:
       return { ...state, showToast: action.payload };
     default:
