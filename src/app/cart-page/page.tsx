7d7ed1743b7cfc78323948b7031daea2fa2e26f9
Tornike Tsagareishvili <dht5152@gmail.com>
2025-06-01 12:26:39 +0400

    Add: Cart Is Empty Image

--- src/app/cart-page/page.tsx
+++ src/app/cart-page/page.tsx
@@ -1,16 +1,24 @@
 "use client";
-
-import Card from "@/components/Card/Card";
-import "../cart-page/CartPage.css";
 import { useGetCartProducts } from "@/hooks/useGetCartProducts";
 import { Product } from "@/hooks/useGetAllProducts";
+
+import Card from "@/components/Card/Card";
 import ProductsSkeleton from "@/components/ProductsSkeleton/ProductsSkeleton";
+import emptyCartImage from "../../../public/images/empty-cart.png";
+import Image from "next/image";
+
+import "../cart-page/CartPage.css";
 
 export default function CartPage() {
   const { cartProducts, loading } = useGetCartProducts();
 
   if (cartProducts?.length === 0) {
-    return <div>Cart is empty</div>;
+    return (
+      <div className="cart-is-empty-container">
+        <h1>Cart is empty</h1>
+        <Image src={emptyCartImage} alt="Cart is empty image" />;
+      </div>
+    );
   }
 
   if (loading) {
