7bdae4f35cddf89f21f500ac9884f8495a8fc39e
Tornike Tsagareishvili <dht5152@gmail.com>
2025-06-06 22:51:50 +0400

    Add: Register page style

--- src/app/register-page/page.tsx
+++ src/app/register-page/page.tsx
@@ -1,5 +1,21 @@
 import "./RegisterPage.css";
 
 export default function Page() {
-  return <div>register</div>;
+  return (
+    <form className="register-container">
+      <input placeholder="Enter username" className="username" type="text" />
+      <input placeholder="Enter email" className="email" type="email" />
+      <input
+        placeholder="Enter password"
+        className="password"
+        type="password"
+      />
+      <input
+        placeholder="Re-Enter password"
+        className="re-password"
+        type="password"
+      />
+      <button className="register-button">Register</button>
+    </form>
+  );
 }
